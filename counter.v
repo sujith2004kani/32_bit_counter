87146ecf55e79e8161e926ea9a57d31cf657b3be
sujith2004kani <77630335+sujith2004kani@users.noreply.github.com>
2025-01-18 13:40:21 +0530

    counter.v

--- counter.v
+++ counter.v
@@ -6,12 +6,8 @@ module counter(
     always@ (posedge clk)
     begin
         if (rst)
-        begin
             count<=0;
-        end
         else
-        begin
             count<=count+1;
-        end
     end
 endmodule
